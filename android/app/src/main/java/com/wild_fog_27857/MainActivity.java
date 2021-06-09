000bbf724f259b5b7552e0ae6ec1f7cce4d4e613
Crowdbotics <team@crowdbotics.com>
2021-06-09 00:47:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/wild_fog_27857/MainActivity.java
@@ -0,0 +1,15 @@
+package com.wild_fog_27857;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "wild_fog_27857";
+  }
+}
